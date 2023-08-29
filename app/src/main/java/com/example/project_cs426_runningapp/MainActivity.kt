067421392922f27e3e556559f886696e530e5fe2
PhucChuHoang <phucchu390@gmail.com>
2023-08-29 23:36:55 +0700

    - Add onboarding screen + navigation bar

--- app/src/main/java/com/example/project_cs426_runningapp/MainActivity.kt
+++ app/src/main/java/com/example/project_cs426_runningapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.project_cs426_runningapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.project_cs426_runningapp.databinding.ActivityOnboardingBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        val navView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        navView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.onboardingFragment) {
+                navView.visibility = View.GONE
+            } else {
+                navView.visibility = View.VISIBLE
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/project_cs426_runningapp/OnboardingFragment.kt
@@ -0,0 +1,41 @@
+package com.example.project_cs426_runningapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.project_cs426_runningapp.databinding.FragmentOnboardingBinding
+
+class OnboardingFragment : Fragment() {
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val clickListener = View.OnClickListener { v ->
+            when (v) {
+                binding.joinCommunityButton -> {
+                    findNavController().navigate(R.id.action_onboardingFragment_to_homeFragment)
+                }
+            }
+        }
+        binding.joinCommunityButton.setOnClickListener(clickListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
