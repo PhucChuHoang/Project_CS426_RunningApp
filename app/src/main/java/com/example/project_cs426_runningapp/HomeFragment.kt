c900285c7ad9a3ffa6f468f916f879b58879ef85
PhucChuHoang <phucchu390@gmail.com>
2023-09-02 20:38:57 +0700

    - Add database

--- app/src/main/java/com/example/project_cs426_runningapp/HomeFragment.kt
+++ app/src/main/java/com/example/project_cs426_runningapp/HomeFragment.kt
@@ -1,24 +1,36 @@
 package com.example.project_cs426_runningapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.project_cs426_runningapp.databinding.FragmentHomeBinding
-import com.example.project_cs426_runningapp.databinding.FragmentLogInBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+        db = FirebaseFirestore.getInstance()
+        var name = ""
+        val email = arguments?.getString("email")
+        if (email != null) {
+            db.collection("users")
+                .whereEqualTo("email", email)
+                .get()
+                .addOnSuccessListener { documents ->
+                    binding.fullName.text = "Hello, " + documents.documents[0].data?.get("fullname").toString()
+                }
+        }
         return binding.root
     }
 
