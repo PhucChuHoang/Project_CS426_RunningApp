f23892a4feec81dc03789fe6dd823fecb2f3d09e
trongnghia2304 <nghiatrongnguyen2304@gmail.com>
2023-09-03 00:44:34 +0700

    Change Activity to Fragment and add Await to HomeFragment

--- app/src/main/java/com/example/project_cs426_runningapp/HomeFragment.kt
+++ app/src/main/java/com/example/project_cs426_runningapp/HomeFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.project_cs426_runningapp.databinding.FragmentHomeBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import android.util.Log
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -22,16 +27,38 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         db = FirebaseFirestore.getInstance()
         val email = arguments?.getString("email")
-        db.collection("users")
-            .whereEqualTo("email", email)
-            .get()
-            .addOnSuccessListener { documents ->
-                for (document in documents) {
-                    binding.fullName.text = "Hello, " + document.data["fullname"].toString()
-                    //TODO: add await
-                }}
+
+        // Use Kotlin Coroutines to perform Firestore operation asynchronously
+        viewLifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
+            try {
+                val querySnapshot = db.collection("users")
+                    .whereEqualTo("email", email)
+                    .get()
+                    .await()
+
+                // Check if there are documents and update the UI
+                if (!querySnapshot.isEmpty) {
+                    Log.d("TAG", "CO TENNNNNN")
+                    val document = querySnapshot.documents[0]
+                    val fullName = document.data?.get("fullname") as? String
+                    fullName?.let {
+                        // Update the UI on the main thread
+                        launch(Dispatchers.Main) {
+                            binding.fullName.text = "Hello, $it"
+                            Log.d("TAG", "Hello, $it")
+                        }
+                    }
+                }
+                else Log.d("TAG", "HONG CO TEN")
+            } catch (e: Exception) {
+                // Handle any exceptions that may occur during the Firestore operation
+                e.printStackTrace()
+            }
+        }
+
         return binding.root
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -41,24 +68,18 @@ class HomeFragment : Fragment() {
                 binding.startCurrentLayout -> {
                     findNavController().navigate(R.id.action_homeFragment_to_runningFragment)
                 }
+                binding.profileImage -> {
+                    findNavController().navigate(R.id.action_homeFragment_to_editProfileFragment)
+                }
+                binding.setting -> {
+                    findNavController().navigate(R.id.action_homeFragment_to_settingFragment)
+                }
             }
         }
 
-        // Add a click listener to the profile_image ImageView
-        binding.profileImage.setOnClickListener {
-            // Start the EditProfileActivity
-            val intent = Intent(requireContext(), EditProfileActivity::class.java)
-            startActivity(intent)
-        }
-
-        // Add a click listener to the profile_image ImageView
-        binding.setting.setOnClickListener {
-            // Start the EditProfileActivity
-            val intent = Intent(requireContext(), SettingActivity::class.java)
-            startActivity(intent)
-        }
-
         binding.startCurrentLayout.setOnClickListener(clickListener)
+        binding.profileImage.setOnClickListener(clickListener)
+        binding.setting.setOnClickListener(clickListener)
     }
 
 }
