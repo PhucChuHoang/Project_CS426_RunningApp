ea14567b85a441e406170365e31acccb4b403c98
quocle <tlquoc03@gmail.com>
2023-09-03 16:44:16 +0700

    fix notification

---
+++ app/src/main/java/com/example/project_cs426_runningapp/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.example.project_cs426_runningapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+}
